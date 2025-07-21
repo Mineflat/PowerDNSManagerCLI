88800c916e6a9a97afe642965b83981106171ded
Mineflat <mineflat@yandex.ru>
2025-07-21 03:13:50 +0300

    Переписал ServerView

--- PowerDNSManagerCLI/UI/ServerView.cs
+++ PowerDNSManagerCLI/UI/ServerView.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PowerDNSManagerCLI.Models;
+using Spectre.Console;
 
-namespace PowerDNSManagerCLI.UI
+namespace PowerDNSManagerCLI.UI;
+
+public static class ServerView
 {
-    internal class ServerView
+    public static void Render(ServerInfo[] servers)
     {
+        if (servers.Length == 0)
+        {
+            AnsiConsole.MarkupLine("[red]Нет доступных серверов для отображения.[/]");
+            return;
+        }
+
+        var table = new Table()
+            .Border(TableBorder.Rounded)
+            .Expand()
+            .Title("[bold blue]Информация о серверах[/]")
+            .AddColumn("ID")
+            .AddColumn("Тип")
+            .AddColumn("Версия")
+            .AddColumn("URL");
+
+        foreach (var server in servers)
+        {
+            table.AddRow(
+                $"[green]{server.Id}[/]",
+                $"[silver]{server.DaemonType}[/]",
+                $"[cyan]{server.Version}[/]",
+                $"[grey]{server.Url}[/]"
+            );
+        }
+
+        AnsiConsole.Write(table);
     }
 }
